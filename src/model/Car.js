e5a142f4bdd165b4b20ad202a4ef3e1c70008fc8
nerdchanii <nerdchanii@gmail.com>
2022-12-15 16:22:02 +0900

    feat(car): 자동차 클래스의 validator추가

--- src/model/Car.js
+++ src/model/Car.js
@@ -1,12 +1,21 @@
+const { MAXIMUM_CAR_NAME } = require("../constants/rule.constants");
+
 class Car {
-  #position = 0;
+  position = 0;
   constructor(name) {
+    this.validator(name);
     this.name = name;
   }
 
   move() {
-    this.#position += 1
+    this.position += 1
   }
+
+
+  validator(name) {
+    if (name.length > MAXIMUM_CAR_NAME) throw new Error('[ERROR]')
+  }
+
 
 }
 
