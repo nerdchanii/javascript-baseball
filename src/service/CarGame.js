e6447650f5e1eef7ea8af4ba5bddfad098e6e1b4
nerdchanii <nerdchanii@gmail.com>
2022-12-15 16:21:28 +0900

    feat(service): winner를 구하는 메소드 추가

--- src/service/CarGame.js
+++ src/service/CarGame.js
@@ -1,12 +1,14 @@
 const Car = require('../model/Car');
 const moveJudgeFn = require('../utils/moveJuge');
-
+const Judge = require('../model/Judge');
 class CarGame {
   constructor() {
+    this.judge = new Judge();
   }
 
-  getCars(names) {
+  makeCars(names) {
     this.cars = names.map((carName) => new Car(carName));
+    this.judge.setCars(this.cars);
   }
 
   playTurn() {
@@ -23,6 +25,11 @@ class CarGame {
     return this.cars.map((car) => {
       return [car.name, car.position];
     })
+  }
+
+
+  getWinners() {
+    return this.judge.getWinners();
   }
 
 }
